cf11ba0f27ef16a095bba343b40ecf56608f8fe1
Kristofer Hallin <kristofer@sunet.se>
2025-02-25 14:27:13 +0100

    Set out path and correct out filename.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use std::process::Command;
 use std::sync::mpsc::{channel, Sender};
 use std::thread;
 
-fn transcoder_thread(path: &str) {
+fn transcoder_thread(path: &str, outpath: &str) {
     let filename = Path::new(path).file_name().unwrap().to_str().unwrap();
-    let outfile = format!("/Users/khn/{}_transcoded.wav", filename);
+    let filename = filename.split('.').next().unwrap();
+    let outfile = format!("{}/{}_transcoded.wav", outpath, filename);
     let output = Command::new("ffmpeg")
         .args([
             "-i",
@@ -30,11 +31,11 @@ fn transcoder_thread(path: &str) {
     }
 }
 
-fn consumer_thread(rx: std::sync::mpsc::Receiver<PathBuf>) {
+fn consumer_thread(rx: std::sync::mpsc::Receiver<PathBuf>, outpath: &str) {
     loop {
         if let Ok(path) = rx.recv() {
             println!("Processing file: {:?}", path);
-            transcoder_thread(path.to_str().unwrap());
+            transcoder_thread(path.to_str().unwrap(), outpath);
             println!("Done processing file: {:?}", path);
         } else {
             println!("Error receiving file path.");
@@ -62,11 +63,15 @@ fn handle_event(event: &notify::Event, tx: &Sender<PathBuf>) {
 }
 
 fn main() -> Result<()> {
-    if std::env::args().len() != 2 {
-        println!("Usage: {} <path>", std::env::args().nth(0).unwrap());
+    if std::env::args().len() != 3 {
+        println!(
+            "Usage: {} <in path> <out path>",
+            std::env::args().nth(0).unwrap()
+        );
         std::process::exit(1);
     }
 
+    let outpath = std::env::args().nth(2).unwrap();
     let (tx, rx) = channel();
     let path = std::env::args().nth(1).unwrap();
     let mut watcher = notify::recommended_watcher(move |res| match res {
@@ -80,7 +85,7 @@ fn main() -> Result<()> {
 
     // Start consumer thread
     thread::spawn(move || {
-        consumer_thread(rx);
+        consumer_thread(rx, &outpath);
     });
 
     loop {
